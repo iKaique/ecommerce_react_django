e72e5c2200a9d053c838d9f00f2fa267bb96d616
Marcelo Kaique de Andrade <kaique.dev@outlook.com>
2021-03-22 22:48:29 -0300

    stars rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
+import Rating from './Rating'
 
 function Product({product}) {
     return (
@@ -8,6 +9,22 @@ function Product({product}) {
                 <Card.Img src={product.image} />
 
             </a>
+            <Card.Body>
+            <a href={`/produc/${product._id}`}>
+                <Card.Title as="div"> 
+                    <strong>{product.name}</strong>
+                </Card.Title>
+            </a>
+            
+            <Card.Text as="div">
+                <div className="my-3">
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'}></Rating>
+                </div>
+            </Card.Text>
+            <Card.Text as="h3">
+                ${product.price}
+            </Card.Text>
+            </Card.Body>
         </Card>
     )
 }
